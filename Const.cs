95789384ee3c39bb5821f0605d0d79b8f1e36856
Vladimir <dementev438@gmail.com>
2021-02-07 23:33:14 +0700

    Добавьте файлы проекта.

---
+++ Const.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matrix
+{
+    static class Const
+    {
+        public const int WIDTH  = 40;    //Ширина поля
+
+        public const int HIGHT  = 20;    //Высота поля
+
+        public const int SIZE   = 30;    //Размер символа
+
+        public const int BGNKTK = 12449; //Начальное значение Unicode Katakana
+
+        public const int ENDKTK = 12449; //Конечное значение Unicode Katakana
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using static Matrix.Const;
+
+namespace Matrix
+{
+    public partial class Form1 : Form
+    {
+        Label[][] labels = new Label[HIGHT][];
+
+        Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            
+            Init();
+
+            timer1.Interval = 200;
+
+            timer1.Tick += new EventHandler(update);
+
+            timer1.Start();
+        }
+
+        private void update(object sender, EventArgs e)
+        {
+            labels[5][4].ForeColor = Color.Red;
+
+            labels[5][4].Text = ((char)rand.Next(BGNKTK, ENDKTK)).ToString();
+        }
+
+        void Init()
+        {
+            for (int Y = 0; Y < labels.Length; Y++)
+            {
+                labels[Y] = new Label[WIDTH];
+
+                for (int X = 0; X < labels[0].Length; X++)
+                {
+                    labels[Y][X]           = new Label();
+                    labels[Y][X].Text      = "f";
+                    labels[Y][X].AutoSize  = true;
+                    labels[Y][X].ForeColor = Color.Green;
+                    labels[Y][X].Location  = new Point(X * SIZE, Y * SIZE);
+                    Controls.Add(labels[Y][X]);
+                }
+            }
+        }
+    }
+}
